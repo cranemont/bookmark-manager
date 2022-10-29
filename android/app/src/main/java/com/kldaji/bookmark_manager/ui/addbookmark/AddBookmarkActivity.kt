835af72ee0ba4292bda3f0fe1a70919aa2709e75
kldaji <kldaji@naver.com>
2022-10-29 10:41:55 +0900

    크롬 공유 기능을 통해 url 송수신

---
+++ android/app/src/main/java/com/kldaji/bookmark_manager/ui/addbookmark/AddBookmarkActivity.kt
@@ -0,0 +1,23 @@
+package com.kldaji.bookmark_manager.ui.addbookmark
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.material.Text
+import com.kldaji.bookmark_manager.ui.theme.BookmarkmanagerTheme
+
+class AddBookmarkActivity : ComponentActivity() {
+
+	override fun onCreate(savedInstanceState: Bundle?) {
+		super.onCreate(savedInstanceState)
+
+		setContent {
+			BookmarkmanagerTheme {
+				intent.getStringExtra(Intent.EXTRA_TEXT)?.let { url ->
+					Text(text = url)
+				}
+			}
+		}
+	}
+}
