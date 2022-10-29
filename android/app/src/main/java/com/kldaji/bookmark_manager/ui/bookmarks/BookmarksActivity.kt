b12be9f861e3cdc2fbb000895f5ac3434aced835
kldaji <kldaji@naver.com>
2022-10-29 13:11:49 +0900

    북마크 카테고리 탭 구현

--- android/app/src/main/java/com/kldaji/bookmark_manager/ui/bookmarks/BookmarksActivity.kt
+++ android/app/src/main/java/com/kldaji/bookmark_manager/ui/bookmarks/BookmarksActivity.kt
@@ -3,24 +3,37 @@ package com.kldaji.bookmark_manager.ui.bookmarks
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
 import androidx.compose.material.icons.filled.History
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
+import com.google.accompanist.pager.ExperimentalPagerApi
+import com.google.accompanist.pager.HorizontalPager
+import com.google.accompanist.pager.pagerTabIndicatorOffset
+import com.google.accompanist.pager.rememberPagerState
 import com.kldaji.bookmark_manager.ui.theme.BookmarkmanagerTheme
+import kotlinx.coroutines.launch
 
 class BookmarksActivity : ComponentActivity() {
 
+	@OptIn(ExperimentalPagerApi::class)
 	override fun onCreate(savedInstanceState: Bundle?) {
 		super.onCreate(savedInstanceState)
 
 		setContent {
 			BookmarkmanagerTheme {
 				val modifier = Modifier
+				val pagerState = rememberPagerState()
+				val coroutineScope = rememberCoroutineScope()
+				val pages = listOf("ALL", "SPORTS", "COMPUTER SCIENCE", "YOUTUBE")
 
 				Scaffold(
 					topBar = {
@@ -50,10 +63,40 @@ class BookmarksActivity : ComponentActivity() {
 					}
 				) { paddingValues ->
 
-					Text(
-						modifier = modifier.padding(paddingValues),
-						text = "body..."
-					)
+					Column(modifier = modifier.padding(paddingValues)) {
+						ScrollableTabRow(
+							selectedTabIndex = pagerState.currentPage,
+							indicator = { tabPositions ->
+								TabRowDefaults.Indicator(modifier.pagerTabIndicatorOffset(pagerState, tabPositions))
+							},
+							backgroundColor = Color.White,
+							edgePadding = 0.dp
+						) {
+							pages.forEachIndexed { index, title ->
+								Tab(
+									text = { Text(text = title) },
+									selected = pagerState.currentPage == index,
+									onClick = {
+										coroutineScope.launch {
+											pagerState.scrollToPage(index)
+										}
+									}
+								)
+							}
+						}
+
+						HorizontalPager(
+							count = pages.size,
+							state = pagerState
+						) { page ->
+							Text(
+								modifier = modifier
+									.fillMaxSize()
+									.background(Color.Gray),
+								text = page.toString()
+							)
+						}
+					}
 				}
 			}
 		}
