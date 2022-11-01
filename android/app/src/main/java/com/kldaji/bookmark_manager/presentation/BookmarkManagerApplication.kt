a828bde0b65b202103cf887e8403f7fb463e58c5
kldaji <kldaji@naver.com>
2022-11-02 00:46:19 +0900

    feat: add hilt annotation

---
+++ android/app/src/main/java/com/kldaji/bookmark_manager/presentation/BookmarkManagerApplication.kt
@@ -0,0 +1,7 @@
+package com.kldaji.bookmark_manager.presentation
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class BookmarkManagerApplication : Application()
