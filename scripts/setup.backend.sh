84300b2e73c3674f5a9203d5f870612dc6ad0559
coc0a25 <cranemont@gmail.com>
2022-11-03 08:46:59 +0000

    chore: add oneai api key to .env automatically on devcontainer

--- scripts/setup.backend.sh
+++ scripts/setup.backend.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 set -ex
 
-npm i -g @nestjs/cli
+# https://www.oneai.com/ 에서 발급받은 API KEY를 입력하세요
+API_KEY="YOUR_API_KEY_HERE"
 
-echo "DATABASE_URL=\"postgresql://postgres:5432@localhost:5432/cabstone?schema=public\"" > backend/.env
+npm i -g @nestjs/cli
+cd backend
+
+yarn install
+
+echo "DATABASE_URL=\"postgresql://postgres:5432@localhost:5432/cabstone?schema=public\"" > .env
+echo "ONE_AI_API_KEY=\"$API_KEY\"" > .env
