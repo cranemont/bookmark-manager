10634da4ddca3ad1d826b504c7ef1b718e83e1df
coc0a25 <cranemont@gmail.com>
2022-11-03 17:34:02 +0000

    chore: change one-ai response dto
    - topics필드를 tag로 변경
    - oneai에서 undefined가 아닌 ''형태로 값이 넘어오는 경우가 있음.
      joi validation시 allow로 해당 값을 처리

--- backend/src/oneai/dto/oneai-response.dto.ts
+++ backend/src/oneai/dto/oneai-response.dto.ts
@@ -4,25 +4,25 @@ import { Output } from 'oneai/lib/src/classes'
 export class SummarizeUrlResponseDto {
   title: string
   thumbnail: string
-  topics: Array<string>
+  tags: Array<string>
   summary: string
 
   constructor(output: Output) {
     this.title = Joi.attempt(
       output.htmlFields.find((field) => field.name === 'title').value,
-      Joi.string().default(''),
+      Joi.string().allow('').default(null),
     )
     this.thumbnail = Joi.attempt(
       output.htmlFields.find((field) => field.name === 'thumbnail').value,
-      Joi.string().default(''),
+      Joi.string().allow('').default(null),
     )
-    this.topics = Joi.attempt(
-      output.topics.map((topic) => topic.value as string),
-      Joi.array().items(Joi.string().default('')).default([]),
+    this.tags = Joi.attempt(
+      output.topics.map((topic) => topic.value),
+      Joi.array().items(Joi.string().allow('')).default([]),
     )
     this.summary = Joi.attempt(
-      output.summary.text as string,
-      Joi.string().default(''),
+      output.summary.text,
+      Joi.string().allow('').default(null),
     )
   }
 }
