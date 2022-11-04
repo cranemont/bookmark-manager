b64c8261bbc272f594757c668741bec5ba1a0ae1
cranemont <cranemont@gmail.com>
2022-11-04 09:49:56 +0000

    fix: change db host in setup script

--- backend/scripts/setup.sh
+++ backend/scripts/setup.sh
@@ -7,7 +7,7 @@ cd $BASEDIR
 # https://www.oneai.com/ 에서 발급받은 API KEY를 입력하세요
 API_KEY="YOUR_API_KEY_HERE"
 
-DB_HOST=localhost
+DB_HOST=app-dev-db
 DB_PORT=27017
 
 npm i -g @nestjs/cli
@@ -16,4 +16,4 @@ yarn prisma generate
 
 rm -f .env
 echo "DATABASE_URL=\"mongodb://mongo:1234@$DB_HOST:$DB_PORT/cabstone\"" > .env
-echo "ONE_AI_API_KEY=\"$API_KEY\"" > .env
+echo "ONE_AI_API_KEY=\"$API_KEY\"" >> .env
