f9b91d7adb86e0e53ed32b36f2d2cb7d06c786a3
cranemont <cranemont@gmail.com>
2022-11-10 17:12:30 +0000

    chore: rename summarize_url_response_dto constructor parameter

--- backend/src/oneai/dto/oneai-response.dto.ts
+++ backend/src/oneai/dto/oneai-response.dto.ts
@@ -4,25 +4,26 @@ import { Output } from 'oneai/lib/src/classes'
 export class SummarizeUrlResponseDto {
   title: string
   thumbnail: string
-  topics: Array<string>
+  tags: Array<string>
   summary: string
 
-  constructor(output: Output) {
+  constructor(oneAIResponse: Output) {
     this.title = Joi.attempt(
-      output.htmlFields.find((field) => field.name === 'title').value,
-      Joi.string().default(''),
+      oneAIResponse.htmlFields.find((field) => field.name === 'title').value,
+      Joi.string().allow('').default(null),
     )
     this.thumbnail = Joi.attempt(
-      output.htmlFields.find((field) => field.name === 'thumbnail').value,
-      Joi.string().default(''),
+      oneAIResponse.htmlFields.find((field) => field.name === 'thumbnail')
+        .value,
+      Joi.string().allow('').default(null),
     )
-    this.topics = Joi.attempt(
-      output.topics.map((topic) => topic.value as string),
-      Joi.array().items(Joi.string().default('')).default([]),
+    this.tags = Joi.attempt(
+      oneAIResponse.topics.map((topic) => topic.value),
+      Joi.array().items(Joi.string().allow('')).default([]),
     )
     this.summary = Joi.attempt(
-      output.summary.text as string,
-      Joi.string().default(''),
+      oneAIResponse.summary.text,
+      Joi.string().allow('').default(null),
     )
   }
 }
