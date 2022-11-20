727973c630438ec76798d9ba73e83760bd8175c7
HakSan <cranemont@gmail.com>
2022-11-20 23:03:18 +0900

    chore: use validationpipe

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,11 +1,14 @@
 import { NestFactory } from '@nestjs/core'
 import { PrismaService } from './prisma/prisma.service'
 import { AppModule } from './app.module'
+import { ValidationPipe } from '@nestjs/common'
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule)
   const prismaService = app.get(PrismaService)
   await prismaService.enableShutdownHooks(app)
+  app.useGlobalPipes(new ValidationPipe())
+
   await app.listen(3000)
 }
 bootstrap()
