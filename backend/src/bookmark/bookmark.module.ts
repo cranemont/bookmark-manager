d4c362eb69f9413aa5f3c548e94fb7b896fb73a6
HakSan <cranemont@gmail.com>
2022-11-20 23:02:20 +0900

    chore: set module options

--- backend/src/bookmark/bookmark.module.ts
+++ backend/src/bookmark/bookmark.module.ts
@@ -3,9 +3,13 @@ import { PrismaService } from 'src/prisma/prisma.service'
 import { BookmarkWebController } from './bookmark-web.controller'
 import { BookmarkMobileController } from './bookmark-mobile.controller'
 import { BookmarkService } from './bookmark.service'
+import { BookmarkRepository } from './bookmark.repository'
+import { UserModule } from 'src/user/user.module'
+import { GroupModule } from 'src/group/group.module'
 
 @Module({
+  imports: [UserModule, GroupModule],
   controllers: [BookmarkWebController, BookmarkMobileController],
-  providers: [BookmarkService, PrismaService],
+  providers: [BookmarkService, PrismaService, BookmarkRepository],
 })
 export class BookmarkModule {}
