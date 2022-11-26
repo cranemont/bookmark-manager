0274a82e0aea738674c01e0a17692827e7a1db8b
Gongmeda <gongmeda@gmail.com>
2022-11-26 13:57:50 +0900

    fix: panel 버튼 누를때마다 매번 새로운 탭이 열리는 이슈 해켤

--- extension/src/pages/background/index.ts
+++ extension/src/pages/background/index.ts
@@ -11,10 +11,16 @@ chrome.runtime.onMessage.addListener(async function (
 ) {
   switch (request.type) {
     case "panel": {
-      await chrome.tabs.create({
-        active: true,
-        url: `chrome-extension://${chrome.runtime.id}/src/pages/panel/index.html`,
-      });
+      const panelUrl = `chrome-extension://${chrome.runtime.id}/src/pages/panel/index.html`;
+      const tabs = await chrome.tabs.query({ url: panelUrl });
+      if (tabs.length > 0) {
+        await chrome.tabs.highlight({ tabs: tabs[0].index });
+      } else {
+        await chrome.tabs.create({
+          active: true,
+          url: panelUrl,
+        });
+      }
       break;
     }
     case "add": {
