317ca5bb7fbddee98c8a7755049bf7a98571053c
coc0a25 <cranemont@gmail.com>
2022-11-29 17:50:57 +0000

    chore: add passport dependencies to auth module

--- backend/src/auth/auth.module.ts
+++ backend/src/auth/auth.module.ts
@@ -1,12 +1,14 @@
 import { Module } from '@nestjs/common'
+import { PassportModule } from '@nestjs/passport'
 import { Hasher } from 'src/common/hasher'
 import { UserModule } from 'src/user/user.module'
 import { AuthController } from './auth.controller'
 import { AuthService } from './auth.service'
+import { LocalStrategy } from './strategy/local.strategy'
 
 @Module({
-  imports: [UserModule],
+  imports: [UserModule, PassportModule.register({ session: true })],
   controllers: [AuthController],
-  providers: [AuthService, Hasher],
+  providers: [AuthService, Hasher, LocalStrategy],
 })
 export class AuthModule {}
