88323ec013c8a800c8fda26bf143bd19e4cc3be4
coc0a25 <cranemont@gmail.com>
2022-11-29 17:50:07 +0000

    chore: apply express session

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -2,12 +2,20 @@ import { NestFactory } from '@nestjs/core'
 import { PrismaService } from './prisma/prisma.service'
 import { AppModule } from './app.module'
 import { ValidationPipe } from '@nestjs/common'
+import session from 'express-session'
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule)
   const prismaService = app.get(PrismaService)
   await prismaService.enableShutdownHooks(app)
   app.useGlobalPipes(new ValidationPipe())
+  app.use(
+    session({
+      secret: 'my-secret',
+      resave: false,
+      saveUninitialized: false,
+    }),
+  )
   app.enableCors()
 
   await app.listen(3000)
